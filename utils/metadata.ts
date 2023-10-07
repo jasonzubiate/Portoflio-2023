e7ea109da8d54b6eb01c7bfd6c82a28d16206642
Jason Zubiate <jason@Jasons-Mac-mini.local>
2023-10-07 09:55:34 -0700

    metadata update

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 export function constructMetaData({
   title = "Jason Zubiate 🤙",
-  description = "Frontend Engineer specializing in React and Next.js. Proficient in crafting responsive and interactive web applications that deliver exceptional user experiences. Explore a collection of my projects showcasing mastery in modern web development and discover how I bring ideas to life with code.",
+  description = "Frontend Engineer specializing in React and Next.js. Explore a collection of my projects showcasing mastery in modern web development and discover how I bring ideas to life with code.",
   image = "/assets/img/portfolio.png",
   icons = "/favicon.ico",
   noIndex = false,
